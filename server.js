13c66478d5826b84d62a6480378e744e47c07f4a
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-09-17 12:07:13 +0430

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
