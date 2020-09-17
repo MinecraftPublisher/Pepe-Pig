0354741f261421af1d8492b187c441dab3629dc9
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-09-17 18:01:55 +0430

    fixed embed bug bug

--- server.js
+++ server.js
@@ -9,38 +9,39 @@ bot.on('ready', () => {
 });
 bot.on('message', msg => {
   var mes = msg.content.split(" ");
-  if(msg.author.bot){
-    return;}
-  else{
-    if(mes[0]=="now"){
+  if (msg.author.bot) {
+    return;
+  }
+  else {
+    if (mes[0] == "now") {
       var run = mes[1];
-      if(run=="ping"){
+      if (run == "ping") {
         msg.channel.send("NOPE");
       }
-      else if(run=="help"){
+      else if (run == "help") {
         const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Help')
-	.setDescription('Lemme explain:\nMy prefix is now, so saying now ping will ping.\n\nCommands:\nping: pong!\nhelp: shows help...? i guess...?')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+          .setColor('#0099ff')
+          .setTitle('Help')
+          .setDescription('Lemme explain:\nMy prefix is now, so saying now ping will ping.\n\nCommands:\nping: pong!\nhelp: shows help...? i guess...?')
+          .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+          .addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+          )
+          .addField('Inline field title', 'Some value here', true)
+          .setImage('https://i.imgur.com/wSTFkRM.png')
+          .setTimestamp()
+          .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
-channel.send(exampleEmbed);
+        msg.channel.send(exampleEmbed);
       }
-      else{
+      else {
         msg.channel.send("ummm...");
       }
     }
-    else{
+    else {
       console.log("regretted");
     }
   }
