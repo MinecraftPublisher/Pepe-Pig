8e4e301fb6d30ad1c7afac198fa44e76a7329d51
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-09-20 12:58:25 +0430

    Update ping.js

--- ping.js
+++ ping.js
@@ -12,5 +12,12 @@ axios.post('https://pepepig.glitch.me/', data)
     }).catch((err) => {
         console.error(err);
     });
+  axios.post('https://Dynamo.dragondev15.repl.run', data)
+    .then((res) => {
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+    }).catch((err) => {
+        console.error(err);
+    });
   setTimeout(get, 5000);
 }
