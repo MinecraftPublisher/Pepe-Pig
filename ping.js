abc9212929a34cb73a269ca39567be81a4e7f0bc
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-09-20 12:02:27 +0430

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,7 +1,14 @@
 var https = require("https");
 get();
 function get(){
-  console.log("exec");
-  https.get("https://pepepig.glitch.me/");
+  request.post(
+    'http://www.yoursite.com/formpage',
+    { json: { key: 'value' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    }
+);
   setTimeout(get, 5000);
 }
