26bff7e097f46562d63ba22d2dd46bc3d115dca0
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-09-20 12:17:10 +0430

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,15 +1,37 @@
 var https = require("https");
-var request = require('request');
+function httpsPost({body, ...options}) {
+    return new Promise((resolve,reject) => {
+        const req = https.request({
+            method: 'POST',
+            ...options,
+        }, res => {
+            const chunks = [];
+            res.on('data', data => chunks.push(data))
+            res.on('end', () => {
+                let body = Buffer.concat(chunks);
+                switch(res.headers['content-type']) {
+                    case 'application/json':
+                        body = JSON.parse(body);
+                        break;
+                }
+                resolve(body)
+            })
+        })
+        req.on('error',reject);
+        if(body) {
+            req.write(body);
+        }
+        req.end();
+    })
+}
 get();
 function get(){
-  request.post(
-    'http://www.yoursite.com/formpage',
-    { json: { key: 'value' } },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-        }
-    }
-);
-  setTimeout(get, 5000);
+  const res = await httpsPost({
+    hostname: 'https://pepepig.glitch.me/',
+    path: '/',
+    body: JSON.stringify({
+        environment: isLive ? 'production' : 'demo',
+    })
+})
+  get();
 }
