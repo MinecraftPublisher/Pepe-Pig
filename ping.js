12b087089d277ad31936660dcbcb1eabba6a975e
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-09-20 12:22:37 +0430

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,37 +1,16 @@
-var https = require("https");
-function httpsPost({body, ...options}) {
-    return new Promise((resolve,reject) => {
-        const req = https.request({
-            method: 'POST',
-            ...options,
-        }, res => {
-            const chunks = [];
-            res.on('data', data => chunks.push(data))
-            res.on('end', () => {
-                let body = Buffer.concat(chunks);
-                switch(res.headers['content-type']) {
-                    case 'application/json':
-                        body = JSON.parse(body);
-                        break;
-                }
-                resolve(body)
-            })
-        })
-        req.on('error',reject);
-        if(body) {
-            req.write(body);
-        }
-        req.end();
-    })
-}
+const axios = require('axios');
+  const data = {
+    name: 'Pepe Pig',
+    job: 'Pepe-Pig-Discord-Bot'
+};
 get();
 function get(){
-  const res = await httpsPost({
-    hostname: 'https://pepepig.glitch.me/',
-    path: '/',
-    body: JSON.stringify({
-        environment: isLive ? 'production' : 'demo',
-    })
-})
+axios.post('https://pepepig.glitch.me/', data)
+    .then((res) => {
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+    }).catch((err) => {
+        console.error(err);
+    });
   get();
 }
